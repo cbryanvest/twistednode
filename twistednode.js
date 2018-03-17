309f80c91e832a78037859d63ca2dea7485a2321
root <root@es1.elastictips.com>
2018-03-17 21:08:01 +0000

    Replaced some hard coded values with variables

--- twistednode.js
+++ twistednode.js
@@ -1,8 +1,8 @@
 var express = require('express')
 , app = express()
 , fs = require('fs')
-, htmlheader = 'header.html'
-, htmlfooter = 'footer.html'
+, html_header = 'header.html'
+, html_footer = 'footer.html'
 , contentpath = '/parts/'
 , encoding = 'utf8'
 , server_port = '4444'
@@ -10,7 +10,7 @@ var express = require('express')
 
 app.use(express.static('static'))
 
-console.log(__dirname+contentpath+htmlheader)
+console.log(__dirname+contentpath+html_header)
 
 app.get('/', function (req, res) {
    console.log('connect')
@@ -29,7 +29,7 @@ function DynamicData(){
 function RenderHTML(html_part,encoding,res){
    //Foreach the LoadHTMLArray Here compresses code
    console.log(html_part)
-   fs.readFile(__dirname+contentpath+'header.html',encoding,function(err,data){
+   fs.readFile(__dirname+contentpath+''+html_header,encoding,function(err,data){
       var send_page = data.replace(/:::sitename:::/g, site_name)
       console.log(send_page)
       res.write(send_page)
@@ -37,7 +37,7 @@ function RenderHTML(html_part,encoding,res){
          var send_page = data.replace(/:::sitename:::/g, site_name)
          console.log(send_page)
          res.write(send_page)
-         fs.readFile(__dirname+contentpath+'footer.html',encoding,function(err,data){
+         fs.readFile(__dirname+contentpath+''+html_header,encoding,function(err,data){
             var send_page = data.replace(/:::sitename:::/g, site_name)
             console.log(send_page)
             res.write(send_page)
