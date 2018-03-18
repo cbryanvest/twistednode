10f9ea96b21b10b393aad9a204c119847cf563ca
root <root@es1.elastictips.com>
2018-03-18 13:32:28 +0000

    Added elasticsearch client node. Created esconnecttion.js to connect to the localhost cluster. Added code to pass variable to content.html page

--- twistednode.js
+++ twistednode.js
@@ -7,6 +7,7 @@ var express = require('express')
 , encoding = 'utf8'
 , server_port = '4444'
 , site_name = 'Twistednode'
+, esclient = require('./esconnection.js')
 
 app.use(express.static('static'))
 
@@ -14,27 +15,31 @@ console.log(__dirname+contentpath+html_header)
 
 app.get('/', function (req, res) {
    console.log('connect')
-   RenderHTML("content.html",encoding,res)
+   esclient.cluster.health({}, function(eserr,esresp,esstatus){
+      console.log(esresp)
+      RenderHTML("content.html",encoding,res,esresp)
+   })
 })
 
 app.listen(server_port, function () {
   console.log(site_name+' started on port '+server_port);
 });
 
-function DynamicData(send_content){
+function DynamicData(send_content,extradata){
    var send_content = send_content.replace(/:::sitename:::/g, site_name)
        send_content = send_content.replace(/:::anothervar:::/g, "This is Another Variable Test")
        send_content = send_content.replace(/:::servertime:::/g,new Date())
+       send_content = send_content.replace(/:::clusterstat:::/g, extradata.status)
    return send_content;
 }
 
-function RenderHTML(html_part,encoding,res){
+function RenderHTML(html_part,encoding,res,extradata){
    fs.readFile(__dirname+contentpath+''+html_header,encoding,function(err,data){
-      res.write(DynamicData(data))
+      res.write(DynamicData(data,extradata))
       fs.readFile(__dirname+contentpath+html_part,encoding,function(err,data){
-         res.write(DynamicData(data))
+         res.write(DynamicData(data,extradata))
          fs.readFile(__dirname+contentpath+''+html_footer,encoding,function(err,data){
-            res.write(DynamicData(data))
+            res.write(DynamicData(data,extradata))
             res.end()
          })
       })
